009c58d63bebff5793057ae6c6f25057830d6a75
Xavier Damman <xdamman@gmail.com>
2024-07-08 01:02:34 +0200

    fix for .well-known

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'content-type',
+            value: 'application/json; charset=utf-8',
+          }
+        ]
+      }]
+    }
+
+};
 
 export default nextConfig;
